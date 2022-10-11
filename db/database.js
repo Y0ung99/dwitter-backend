126c9539b63fb0db8207e2f2d95a400355d2863c
Y0ung99 <ancolor@knou.ac.kr>
2022-10-12 00:27:06 +0900

    mysql -> postgres

--- db/database.js
+++ db/database.js
@@ -1,9 +1,10 @@
 import { config } from '../config.js';
 import SQ from 'sequelize';
 
-const {host, user, database, password} = config.db;
+const {host, user, port, database, password} = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {
     host,
-    dialect: 'mysql',
+    port,
+    dialect: 'postgres',
     logging: false,
 })
