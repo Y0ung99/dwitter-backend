72fa07fc7fb26b1e81968fe311daf93a6551ba96
Y0ung99 <ancolor@knou.ac.kr>
2022-10-13 01:35:20 +0900

    remove debug

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,6 @@ const AUTH_ERROR = { message: 'Authentication Error'};
 export const isAuth = async (req, res, next) => {
     const authHeader = req.get('Authorization');
     if (!(authHeader && authHeader.startsWith('Bearer '))) {
-        console.log('1', authHeader);
         return res.status(401).json(AUTH_ERROR);
     }
 
@@ -17,12 +16,10 @@ export const isAuth = async (req, res, next) => {
         config.jwt.secretKey,
         async (error, decoded) => {
             if (error) {
-                console.log('2', error);
                 return res.status(401).json(AUTH_ERROR);
             }
             const user = await userRepository.findById(decoded.id);
             if (!user) {
-                console.log('3', authHeader);
                 return res.status(401).json(AUTH_ERROR);
             }
             req.userId = user.id; // req.customdata
